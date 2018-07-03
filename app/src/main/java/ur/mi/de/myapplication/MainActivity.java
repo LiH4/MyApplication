b6c7cb200fa16c34a6a08e846984e86baa5e1f23
Lisa Hampel <lisa_hampel@t-online.de>
2018-07-03 15:25:47 +0200

    add calculation

--- app/src/main/java/ur/mi/de/myapplication/MainActivity.java
+++ app/src/main/java/ur/mi/de/myapplication/MainActivity.java
@@ -9,5 +9,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int number1 = 2;
+        int number2 = 5;
+
+        int sum = number1 + number2;
     }
 }
